db4b2d48a9d499d7c88922ec44b8b6ea04cbf9d5
Olyxz16 <cedric.sazos@tutanota.com>
2026-03-28 16:57:46 +0100

    feat: add redis for rate limiting

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -8,15 +8,11 @@ import (
 	"github.com/Olyxz16/go-chi-oauth-psql/internal/auth/repositories"
 	"github.com/Olyxz16/go-chi-oauth-psql/internal/auth/services"
 	"github.com/Olyxz16/go-chi-oauth-psql/internal/config"
+	"github.com/go-redis/redis_rate/v10"
 	"go.uber.org/zap"
 )
 
 func main() {
-	gothConf := config.NewGothConfig()
-	config.SetupGoth(gothConf)
-
-	cfg := config.NewServerConfig()
-
 	logger := config.DefaultLogger()
 
 	pgCfg := config.NewPostgresConfig()
@@ -26,13 +22,26 @@ func main() {
 	}
 	defer pool.Close()
 
+	redisCfg := config.NewRedisConfig()
+	redisClient, err := config.NewRedisClient(redisCfg)
+	if err != nil {
+		logger.Fatal("Failed to connect to redis", zap.Error(err))
+	}
+	defer redisClient.Close()
+
+	limiter := redis_rate.NewLimiter(redisClient)
+
+	gothConf := config.NewGothConfig()
+	config.SetupGoth(gothConf)
+
+	cfg := config.NewServerConfig()
 	userRepo := repositories.NewUserRepository(pool)
 	userService := services.NewUserService(userRepo)
 	tokenService := services.NewTokenService(cfg)
 
 	server := &http.Server{
 		Addr:    fmt.Sprintf("%s:%d", cfg.Host, cfg.Port),
-		Handler: api.RegisterRoutes(userService, tokenService, gothConf.GoogleAccessKeyId),
+		Handler: api.RegisterRoutes(userService, tokenService, gothConf.GoogleAccessKeyId, limiter),
 	}
 
 	if err = server.ListenAndServe() ; err != nil {
